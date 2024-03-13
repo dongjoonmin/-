1cf59f800384e5263aae1ac3f47ddcb9230c4c4c
dongjoonmin <134471995+dongjoonmin@users.noreply.github.com>
2024-03-13 13:36:02 +0900

    Update product_script.js

--- 90bc5673ea37412f8c5506d377a2ec60/1 7791a2b1b8d34657a03084c4393cc41a/product_script.js
+++ 90bc5673ea37412f8c5506d377a2ec60/1 7791a2b1b8d34657a03084c4393cc41a/product_script.js
@@ -3,12 +3,15 @@ document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
 });
 
-// 개발자 도구가 열리면 Google로 리디렉션
-window.addEventListener('devtoolschange', function(event) {
-    if (event.detail.isOpen) {
+let screenWidth = window.screen.width;
+
+window.addEventListener('resize', function() {
+    // 개발자 도구가 열리면 화면 너비가 변화됨
+    if (window.screen.width < screenWidth) {
         window.location.href = 'https://www.google.com';
     }
 });
+
 
 // 이미지 클릭 차단
 document.querySelectorAll('.image img').forEach(function(image) {
