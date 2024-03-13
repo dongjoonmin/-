1a9ee428314ee99fa84a8f4dd6ae2f7dcea0a3bd
dongjoonmin <134471995+dongjoonmin@users.noreply.github.com>
2024-03-13 13:47:05 +0900

    Update product_script.js

--- 90bc5673ea37412f8c5506d377a2ec60/1 7791a2b1b8d34657a03084c4393cc41a/product_script.js
+++ 90bc5673ea37412f8c5506d377a2ec60/1 7791a2b1b8d34657a03084c4393cc41a/product_script.js
@@ -3,6 +3,16 @@ document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
 });
 
+
+// 이미지 클릭 차단
+document.querySelectorAll('.image img').forEach(function(image) {
+    image.addEventListener('click', function(event) {
+        event.preventDefault(); // 클릭 이벤트의 기본 동작 방지
+        event.stopPropagation(); // 이벤트 버블링 중지
+        console.log('이미지가 클릭되었습니다.');
+    });
+});
+
 // 개발자 도구 감지
 function detectDevTool(allow) {
     if (isNaN(+allow)) allow = 100;
@@ -34,13 +44,3 @@ function setupDevToolDetection() {
 }
 
 setupDevToolDetection();
-
-
-// 이미지 클릭 차단
-document.querySelectorAll('.image img').forEach(function(image) {
-    image.addEventListener('click', function(event) {
-        event.preventDefault(); // 클릭 이벤트의 기본 동작 방지
-        event.stopPropagation(); // 이벤트 버블링 중지
-        console.log('이미지가 클릭되었습니다.');
-    });
-});
