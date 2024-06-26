3fe1de67894a2e74b4b2a2b76146f9745d9ebeb3
dongjoon-for-fitamin <134471995+dongjoonmin@users.noreply.github.com>
2024-06-26 17:46:27 +0900

    Update main_script.js

--- main_script.js
+++ main_script.js
@@ -5,13 +5,16 @@ document.addEventListener('contextmenu', function(e) {
 
 
 // 이미지 클릭 차단
-document.querySelectorAll('.image img').forEach(function(image) {
-    image.addEventListener('click', function(event) {
-        event.preventDefault(); // 클릭 이벤트의 기본 동작 방지
-        event.stopPropagation(); // 이벤트 버블링 중지
-        console.log('이미지가 클릭되었습니다.');
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.image img').forEach(function(image) {
+        image.addEventListener('click', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            console.log('이미지가 클릭되었습니다.');
+        });
     });
 });
+
 
 
  ! function() {
